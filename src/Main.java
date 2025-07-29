00578dbafd08b79160bf72cd5a5662363649dab9
Diana Askandarova <askandarovadiana@gmail.com>
2025-07-29 20:06:34 +0300

    Working with numbers

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста:" + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sumNum = firstNumber + secondNumber;
+        System.out.println("Сумма чисел = " + sumNum);
+        int razNum = firstNumber - secondNumber;
+        System.out.println("Разность чисел = " + razNum);
+        int mulNum = firstNumber * secondNumber;
+        System.out.println("Произведение чисел = " + mulNum);
+        double divNum = (double) firstNumber / secondNumber;
+        System.out.println("Частное чисел = " + divNum);
     }
 }
